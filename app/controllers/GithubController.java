ef1b1c52259d6d692a1edc2c28c8a9d85c5f22af
Ashwin Raghunath <ashwinr2296@gmail.com>
2021-11-03 11:09:24 -0400

    Project setup, Controller and service class added

---
+++ app/controllers/GithubController.java
@@ -0,0 +1,72 @@
+package controllers;
+
+import org.eclipse.egit.github.core.Repository;
+import play.data.DynamicForm;
+import play.data.FormFactory;
+import play.mvc.*;
+import javax.inject.Inject;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CompletionStage;
+
+import services.GithubService;
+import views.html.index;
+
+import static play.libs.Json.toJson;
+import static play.libs.Scala.asScala;
+import static play.mvc.Results.ok;
+
+public class GithubController {
+	
+
+	 private final FormFactory formFactory;
+	 private final GithubService githubService;
+
+	 @Inject
+	 public GithubController(FormFactory formFactory, GithubService githubService)
+	 {
+		 this.formFactory=formFactory;
+		 this.githubService=githubService;
+	 }
+
+	public Result index() {
+		return ok(views.html.index.render());
+	}
+
+	public Result search(Http.Request request)
+	{
+		DynamicForm form = formFactory.form().bindFromRequest(request);
+		String phrase = form.get("phrase");
+		return ok(views.html.index.render());
+	}
+
+	public Result getSearchResults()
+	{
+		List<String> strings = Arrays.asList("value1", "value2", "value3");
+		return ok(toJson(strings));
+	}
+
+	public CompletionStage<Result> getRepositoryDetails(String userName, String repositoryName)
+	{
+
+		CompletionStage<Result> repositoryDetail = githubService
+				.getRepositoryDetails(userName, repositoryName)
+				.thenApply(repository -> ok(toJson(repository)));
+		return repositoryDetail;
+	}
+
+
+
+
+	public CompletionStage<Result> getRepositoryDetailsRender(String userName, String repositoryName)
+	{
+
+		CompletionStage<Result> resultCompletionStage = githubService
+				.getRepositoryDetails(userName, repositoryName)
+				.thenApply(repository -> ok(views.html.repository.render(repository)));
+		return resultCompletionStage;
+	}
+
+}
+
+
---
+++ app/services/GithubService.java
@@ -0,0 +1,79 @@
+package services;
+
+import models.RepositoryDetails;
+import org.eclipse.egit.github.core.Issue;
+import org.eclipse.egit.github.core.Repository;
+import org.eclipse.egit.github.core.User;
+import org.eclipse.egit.github.core.client.GitHubClient;
+import org.eclipse.egit.github.core.client.PageIterator;
+import org.eclipse.egit.github.core.service.CollaboratorService;
+import org.eclipse.egit.github.core.service.IssueService;
+import org.eclipse.egit.github.core.service.RepositoryService;
+
+import javax.inject.Inject;
+import java.io.IOException;
+import java.util.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+
+public class GithubService {
+
+    private final RepositoryService repositoryService;
+    private final CollaboratorService collaboratorService;
+    private final IssueService issueService;
+    private final GitHubClient gitHubClient;
+
+    public GithubService() {
+        gitHubClient = new GitHubClient();
+        gitHubClient.setCredentials("ashraghunath","Bangalore@12345!");
+        this.repositoryService=new RepositoryService(gitHubClient);
+        this.collaboratorService=new CollaboratorService(gitHubClient);
+        this.issueService=new IssueService(gitHubClient);
+    }
+
+//    public CompletionStage<Repository> getRepositoryDetails(String userName, String repositoryName){
+//
+//        return CompletableFuture.supplyAsync( () -> {
+//            GitHubClient client = new GitHubClient();
+//            RepositoryService repositoryService = new RepositoryService(client);
+//
+//            Repository repository=null;
+//            try {
+//                repository = repositoryService.getRepository(userName, repositoryName);
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//            return repository;
+//        });
+//
+//    }
+
+    public CompletionStage<RepositoryDetails> getRepositoryDetails(String userName, String repositoryName){
+
+        return CompletableFuture.supplyAsync( () -> {
+            RepositoryDetails repositoryDetails = new RepositoryDetails();
+            Repository repository=null;
+            Map<String, String> params = new HashMap<String, String>();
+            params.put(IssueService.FILTER_STATE, "all");
+            try {
+                repository = repositoryService.getRepository(userName, repositoryName);
+                PageIterator<Issue> iterator = issueService.pageIssues(userName, repositoryName,
+                        params, 1);
+                List<Issue> issues = new ArrayList<>();
+                while(iterator.hasNext() && issues.size()!=20)
+                {
+                    issues.add(iterator.next().iterator().next());
+                }
+                repositoryDetails.setRepository(repository);
+                repositoryDetails.setIssues(issues);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return repositoryDetails;
+        });
+
+    }
+
+
+}
