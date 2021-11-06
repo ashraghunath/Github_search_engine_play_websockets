7a49371a0abf0ecaa861773de0ed634cdb3ad08e
anushkashetty96 <anushkashetty56@gmail.com>
2021-11-06 13:21:55 -0400

    task c repository issues endpoint added (#2)

--- app/controllers/GithubController.java
+++ app/controllers/GithubController.java
@@ -15,49 +15,51 @@ import static play.libs.Json.toJson;
 import static play.mvc.Results.ok;
 
 public class GithubController {
-	
 
-	 private final FormFactory formFactory;
-	 private final GithubService githubService;
+	private final FormFactory formFactory;
+	private final GithubService githubService;
 
-	 @Inject
-	 public GithubController(FormFactory formFactory, GithubService githubService)
-	 {
-		 this.formFactory=formFactory;
-		 this.githubService=githubService;
-	 }
+	@Inject
+	public GithubController(FormFactory formFactory, GithubService githubService) {
+		this.formFactory = formFactory;
+		this.githubService = githubService;
+	}
 
 	public Result index() {
 		return ok(views.html.index.render());
 	}
 
-	public Result search(Http.Request request)
-	{
+	public Result search(Http.Request request) {
 		DynamicForm form = formFactory.form().bindFromRequest(request);
 		String phrase = form.get("phrase");
 		return ok(views.html.index.render());
 	}
 
-	public Result getSearchResults()
-	{
+	public Result getSearchResults() {
 		List<String> strings = Arrays.asList("value1", "value2", "value3");
 		return ok(toJson(strings));
 	}
 
-	/** Returns the Repository details for the provided username and repository name
+	/**
+	 * Returns the Repository details for the provided username and repository name
+	 * 
 	 * @author Ashwin Raghunath 40192120
-	 * @param userName the user who owns the repository.
+	 * @param userName       the user who owns the repository.
 	 * @param repositoryName the name of the repository to be searched for.
-	 * @return CompletionStage<Result> represents the async response containing the process stage of Result object
+	 * @return CompletionStage<Result> represents the async response containing the
+	 *         process stage of Result object
 	 */
-	public CompletionStage<Result> getRepositoryDetails(String userName, String repositoryName)
-	{
-		CompletionStage<Result> resultCompletionStage = githubService
-				.getRepositoryDetails(userName, repositoryName)
+	public CompletionStage<Result> getRepositoryDetails(String userName, String repositoryName) {
+		CompletionStage<Result> resultCompletionStage = githubService.getRepositoryDetails(userName, repositoryName)
 				.thenApply(repository -> ok(views.html.repository.render(repository)));
 		return resultCompletionStage;
 	}
 
+	public CompletionStage<Result> getIssues(String userName, String repositoryName) {
+		CompletionStage<Result> resultCompletionStage = githubService.getAllIssues(userName, repositoryName)
+				.thenApply(issues -> ok(views.html.issues.render(issues)));
+
+		return resultCompletionStage;
+	}
+
 }
-
-
