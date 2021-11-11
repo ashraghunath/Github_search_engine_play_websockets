98b38ff82e46764a947048898d866d57a9f47890
anushkashetty96 <anushkashetty56@gmail.com>
2021-11-11 16:46:04 -0500

    tests for getAllIssues

--- test/controllers/GithubControllerTest.java
+++ test/controllers/GithubControllerTest.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import models.IssueWordStatistics;
 import models.RepositoryDetails;
 import org.apache.http.HttpStatus;
 import org.eclipse.egit.github.core.Repository;
@@ -16,6 +17,8 @@ import play.test.Helpers;
 import play.test.WithApplication;
 import services.GithubService;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ExecutionException;
@@ -87,5 +90,50 @@ public class GithubControllerTest extends WithApplication {
             return repositoryDetails;
         });
     }
+    
+    
+    /** Unit test for resting the endpoint /getRepositoryIssues/:userName/:repositoryName   
+     * @author Anushka Shetty 40192371
+     */
+    @Test
+    public void getRepositoryIssuesTest()
+    {
+        running(provideApplication(), () -> {
+            when(githubService.getAllIssues(anyString(),anyString())).thenReturn(issueStatistics());
+            CompletionStage<Result> issueStatistics = githubController.getIssues("play", "play");
+            try {
+                Result result = issueStatistics.toCompletableFuture().get();
+                assertEquals(HttpStatus.SC_OK,result.status());
+                assertEquals("text/html",result.contentType().get());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+    
+    /** mock object for testing getRepositoryIssuesTest
+     * @author Anushka Shetty 40192371
+     * @return CompletionStage<IssueWordStatistics> represents the async response containing the process stage of IssueWordStatistics object
+     */
+    
+    private CompletionStage<IssueWordStatistics> issueStatistics(){
+        return CompletableFuture.supplyAsync( () -> {
+        	Map<String, Integer> mapItem = new HashMap<String, Integer>();        	
+        	mapItem.put("null",2);
+        	mapItem.put("reference",1);
+        	mapItem.put("exception",1);
+        	mapItem.put("pointer",1);
+        	mapItem.put("java",1);
+        	mapItem.put("array",1);
+        	mapItem.put("bound",1);
+        	mapItem.put("index",1);	
+        	mapItem.put("out",1);        
+        	IssueWordStatistics issueWordStatistics = new IssueWordStatistics(mapItem);
+            return issueWordStatistics;
+        });
+    }
+
 
 }
