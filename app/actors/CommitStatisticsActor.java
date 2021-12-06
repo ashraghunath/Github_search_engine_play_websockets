9edf2caceedb45f53fe1df6ae0b260ba9d75dc6f
anmolmalhotra97 <anmolmalhotra97@gmail.com>
2021-12-06 16:00:55 -0500

    Created Commit Stats Actor

---
+++ app/actors/CommitStatisticsActor.java
@@ -0,0 +1,54 @@
+package actors;
+
+import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.actor.Props;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import services.GithubService;
+
+import java.util.concurrent.CompletionStage;
+
+public class CommitStatisticsActor extends AbstractActor {
+
+    private ActorRef supervisorActor;
+    private GithubService githubService;
+
+    public CommitStatisticsActor(ActorRef supervisorActor, GithubService githubService) {
+        this.supervisorActor = supervisorActor;
+        this.githubService = githubService;
+    }
+
+    public static Props props(ActorRef supervisorActor, GithubService githubService) {
+        return Props.create(RepositoryDetailsActor.class, supervisorActor, githubService);
+    }
+
+    @Override
+    public void preStart() {
+        System.out.println("Commit Statistics Actor created");
+    }
+
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder().match(Messages.GetCommitStatisticsActor.class, commitStats -> getCommitStatistics(commitStats).thenAcceptAsync(this::processCommitStatisticsResult)).build();
+    }
+
+    private CompletionStage<JsonNode> getCommitStatistics(Messages.GetCommitStatisticsActor commitStatsActor) {
+        return githubService.getCommitStatisticsForRepository(commitStatsActor.username, commitStatsActor.repositoryName)
+                .thenApplyAsync(commitStatsData -> {
+                    ObjectMapper objectMapper = new ObjectMapper();
+                    ObjectNode commitStatObjectNode = objectMapper.createObjectNode();
+                    JsonNode commitStatsJsonNode = objectMapper.convertValue(commitStatsData, JsonNode.class);
+
+                    commitStatObjectNode.put("responseType", "commitStatisticsPage");
+                    commitStatObjectNode.set("commitStatsList", commitStatsJsonNode);
+                    commitStatObjectNode.put("repositoryName", commitStatsActor.repositoryName);
+                    return commitStatObjectNode;
+                });
+    }
+
+    private void processCommitStatisticsResult(JsonNode commitStatistics) {
+        supervisorActor.tell(new Messages.IssueStatistics(commitStatistics), getSelf());
+    }
+}
